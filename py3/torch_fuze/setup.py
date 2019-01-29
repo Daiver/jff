ef7399a16367012f0db74fb8484bb3a2840849d8
Kirill Klimov <ra22341@ya.ru>
2019-01-29 16:14:49 +0300

    minor changes

---
+++ py3/torch_fuze/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="torch_fuze",
+    version="0.0.1",
+    author="Daiver",
+    author_email="ra22341@ya.ru",
+    description="Yet another PyTorch training framework",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
