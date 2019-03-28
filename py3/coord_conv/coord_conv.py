18bc6a3a91db508cb72a3bb91a58393029857aab
Daiver <ra22341@ya.ru>
2019-03-28 09:27:23 +0300

    Improved coord conv example

--- py3/coord_conv/coord_conv.py
+++ py3/coord_conv/coord_conv.py
@@ -5,8 +5,8 @@ import torch.nn.functional as F
 
 class CoordConv2d(nn.Module):
     def __init__(self,
-                 in_channels, out_channels, kernel_size, stride,
-                 padding, dilation, groups, bias):
+                 in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True):
         super().__init__()
         print("WARNING: Not tested at all")
         self.conv = nn.Conv2d(
@@ -14,19 +14,24 @@ class CoordConv2d(nn.Module):
             padding=padding, dilation=dilation, groups=groups, bias=bias
         )
         self.coord_feature_map = None
-        assert False
 
-    def init_coord_feature_map(self, required_rows_cols):
+    def init_coord_feature_map(self, required_batch_rows_cols):
         if self.coord_feature_map is not None:
-            assert self.coord_feature_map.shape[2] == required_rows_cols[0]
-            assert self.coord_feature_map.shape[3] == required_rows_cols[1]
-            return
+            if (self.coord_feature_map.shape[0] == required_batch_rows_cols[0] and
+                    self.coord_feature_map.shape[2] == required_batch_rows_cols[1] and
+                    self.coord_feature_map.shape[3] == required_batch_rows_cols[2]):
+                return
 
         theta = torch.FloatTensor([1, 0, 0, 0, 1, 0]).view(1, 2, 3)
+        theta = torch.cat((theta, ) * required_batch_rows_cols[0], dim=0)
+
         self.coord_feature_map = \
-            F.affine_grid(theta, (1, 1, required_rows_cols[0], required_rows_cols[1])).transpose(1, 3)
+            F.affine_grid(theta,
+                          (required_batch_rows_cols[0], 1, required_batch_rows_cols[1], required_batch_rows_cols[2]))\
+                .transpose(1, 3).to("cuda:0")
 
     def forward(self, x):
-        self.init_coord_feature_map((x.shape[2], x.shape[3]))
-        x = torch.cat((x, self.coord_feature_map), dim=1)
+        self.init_coord_feature_map((x.shape[0], x.shape[2], x.shape[3]))
+        # print(f"x.shape {x.shape}, self.coord.shape {self.coord_feature_map.shape}")
+        x = torch.cat((self.coord_feature_map, x), dim=1)
         return self.conv(x)
--- py3/coord_conv/main.py
+++ py3/coord_conv/main.py
@@ -1,4 +1,7 @@
 from collections import OrderedDict
+import random
+import cv2
+import numpy as np
 import torch
 import torch_fuze
 
@@ -9,19 +12,92 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 import torchvision.transforms as transforms
 
+from coord_conv import CoordConv2d
 
-def mk_dataset():
 
-    pass
+canvas_size = (32, 32)
+
+
+def mk_point_sample():
+    canvas = np.zeros((canvas_size[0], canvas_size[1], 3), dtype=np.uint8)
+    coord = (random.randint(0, canvas_size[0] - 1), random.randint(0, canvas_size[1] - 1))
+    canvas[coord[0], coord[1]] = (0, 255, 0)
+    return canvas, coord
+
+
+def mk_points_dataset(n_samples):
+    return [mk_point_sample() for _ in range(n_samples)]
 
 
 def main():
+    torch_fuze.utils.manual_seed(42)
+    n_samples = 200
+    dataset = mk_points_dataset(n_samples)
+    for img, target in dataset:
+        print(target)
+        cv2.imshow('', cv2.pyrUp(cv2.pyrUp(img)))
+        cv2.waitKey()
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(3, 16, 5, stride=2, padding=2),  # 32 -> 16
+            nn.ReLU(True),
+            nn.Conv2d(16, 32, 5, stride=2, padding=2),  # 16 -> 8
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 3, stride=2, padding=1),  # 8 -> 4
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 3, stride=2, padding=1),  # 4 -> 2
+            nn.ReLU(True),
+        )
+
+        self.fc = nn.Linear(2 * 2 * 32, 2)
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = x.view(-1, 2 * 2 * 32)
+        x = self.fc(x)
+        return x
+
+
+class Net2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.feature_extractor = nn.Sequential(
+            CoordConv2d(3, 16, 5, stride=2, padding=2),  # 32 -> 16
+            nn.ReLU(True),
+            CoordConv2d(16, 32, 5, stride=2, padding=2),  # 16 -> 8
+            nn.ReLU(True),
+            CoordConv2d(32, 32, 3, stride=2, padding=1),  # 8 -> 4
+            nn.ReLU(True),
+            CoordConv2d(32, 32, 3, stride=2, padding=1),  # 4 -> 2
+            nn.ReLU(True),
+        )
+
+        self.fc = nn.Linear(2 * 2 * 32, 2)
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = x.view(-1, 2 * 2 * 32)
+        x = self.fc(x)
+        return x
+
+
+def main_train():
     print(f"Torch version: {torch.__version__}, CUDA: {torch.version.cuda}, Fuze version: {torch_fuze.__version__}")
+
+    torch_fuze.utils.manual_seed(42)
+    n_train_samples = 1000
+    n_test_samples = 200
+    train_set = mk_points_dataset(n_train_samples)
+    test_set = mk_points_dataset(n_test_samples)
 
     # lr = 0.01
     # batch_size = 32
-    batch_size = 64
-    # batch_size = 128
+    # batch_size = 64
+    batch_size = 256
     # device = "cpu"
     device = "cuda:0"
 
@@ -29,25 +105,25 @@ def main():
     if device.startswith("cuda"):
         print(f"GPU name: {torch.cuda.get_device_name(int(device.split(':')[-1]))}")
 
-    trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
-    # if not exist, download mnist dataset
-    train_set = CIFAR10(root="data/", train=True, transform=trans, download=True)
-    test_set = CIFAR10(root="data/", train=False, transform=trans, download=True)
+    inp_trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
+    out_trans = torch.FloatTensor
 
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, pin_memory=False, num_workers=4)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, pin_memory=False, num_workers=4)
+    train_loader = DataLoader(torch_fuze.data.InputOutputTransformsWrapper(train_set, inp_trans, out_trans),
+                              batch_size=batch_size, shuffle=True, pin_memory=False, num_workers=4)
+    test_loader = DataLoader(torch_fuze.data.InputOutputTransformsWrapper(test_set, inp_trans, out_trans),
+                             batch_size=batch_size, shuffle=False, pin_memory=False, num_workers=4)
 
-    # model = Net()
-    model = Net2()
+    model = Net()
+    # model = Net2()
     model = nn.DataParallel(model)
     model.to(device)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters())
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[5, 8], gamma=0.3)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 30], gamma=0.2)
 
     metrics = OrderedDict([
         ("loss", criterion),
-        ("acc", torch_fuze.metrics.Accuracy())
+        # ("acc", torch_fuze.metrics.Accuracy())
     ])
     callbacks = [
         torch_fuze.callbacks.ProgressCallback(),
@@ -58,4 +134,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    main_train()
