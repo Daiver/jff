80a7eb42a9be783f66460243ab9cc2fd7bbc135d
Kirill Klimov <ra22341@ya.ru>
2019-04-12 14:27:00 +0300

    minor changes

--- py3/rasterizer1/geom_tools/tests/test_obj_export.py
+++ py3/rasterizer1/geom_tools/tests/test_obj_export.py
@@ -12,7 +12,7 @@ class TestObjParser(unittest.TestCase):
                 [1, 0, 0],
                 [0, 1, 0],
                 [0, 0, 1],
-            ]),
+            ], dtype=np.float32),
             polygon_vertex_indices=[
                 [0, 1, 2]
             ],
@@ -22,11 +22,39 @@ class TestObjParser(unittest.TestCase):
         geom_tools.save_to_stream(stream, mesh)
         res = stream.getvalue()
         ans = """\
-v 1 0 0
-v 0 1 0
-v 0 0 1
+v 1.0 0.0 0.0
+v 0.0 1.0 0.0
+v 0.0 0.0 1.0
 
 f 1 2 3
+"""
+        self.assertEqual(res, ans)
+
+    def test_mesh_obj_export02(self):
+        mesh = geom_tools.Mesh(
+            vertices=np.array([
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1],
+                [5, 6, 7.5],
+            ]),
+            polygon_vertex_indices=[
+                [0, 1, 2, 3],
+                [3, 1, 2],
+            ],
+        )
+
+        stream = io.StringIO()
+        geom_tools.save_to_stream(stream, mesh)
+        res = stream.getvalue()
+        ans = """\
+v 1.0 0.0 0.0
+v 0.0 1.0 0.0
+v 0.0 0.0 1.0
+v 5.0 6.0 7.5
+
+f 1 2 3 4
+f 4 2 3
 """
         self.assertEqual(res, ans)
 
