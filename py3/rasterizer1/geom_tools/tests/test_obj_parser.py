e980c7003612a82136a2175c48c44103eea5803a
Kirill Klimov <ra22341@ya.ru>
2019-04-12 12:47:58 +0300

    minor changes

--- py3/rasterizer1/geom_tools/tests/test_obj_parser.py
+++ py3/rasterizer1/geom_tools/tests/test_obj_parser.py
@@ -49,6 +49,70 @@ class TestObjParser(unittest.TestCase):
         )
         self.assertTrue(res == ans)
 
+    def test_from_obj_string03(self):
+        content = """
+        v 1 0 0
+        v 0 1 0
+        v 0 0 1
+        v 16 3 2
+        
+        vn 1 0 0
+
+        f 1//1 2//1 3//1 4//1
+
+        """
+        res = geom_tools.from_obj_string(content)
+        ans = geom_tools.Mesh(
+            vertices=np.array([
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1],
+                [16, 3, 2],
+            ]),
+            polygon_vertex_indices=[[0, 1, 2, 3]],
+            triangle_vertex_indices=[[0, 1, 2], [0, 2, 3]]
+        )
+        self.assertTrue(res == ans)
+
+    def test_from_obj_string04(self):
+        content = """
+        v 1 0 0
+        v 0 1 0
+        v 0 0 1
+        v 16 3 2
+
+        vt 0.5 0
+        vt 0.5 0.5
+        vt 0 0.5
+
+        f 1/1 2/2 3/3 4/1
+
+        """
+        res = geom_tools.from_obj_string(content)
+        ans = geom_tools.Mesh(
+            vertices=np.array([
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1],
+                [16, 3, 2],
+            ]),
+            polygon_vertex_indices=[[0, 1, 2, 3]],
+            texture_vertices=np.array([
+                [0.5, 0],
+                [0.5, 0.5],
+                [0, 0.5]
+            ]),
+            texture_polygon_vertex_indices=[
+                [0, 1, 2, 0]
+            ],
+            triangle_vertex_indices=[[0, 1, 2], [0, 2, 3]],
+            triangle_texture_vertex_indices=[
+                [0, 1, 2],
+                [0, 2, 0]
+            ],
+        )
+        self.assertTrue(res == ans)
+
 
 if __name__ == '__main__':
     unittest.main()
