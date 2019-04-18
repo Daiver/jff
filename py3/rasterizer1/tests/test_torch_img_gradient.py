b44b228678f2aba7d06aa5bfdce60be9268d9f90
Daiver <ra22341@ya.ru>
2019-04-18 08:40:22 +0300

    1

--- py3/rasterizer1/tests/test_torch_img_gradient.py
+++ py3/rasterizer1/tests/test_torch_img_gradient.py
@@ -80,6 +80,24 @@ class TorchImgGradient(unittest.TestCase):
         ])
         self.assertTrue((res - ans).norm() < 1e-6)
 
+    def test_img_dy01(self):
+        img = torch.FloatTensor([
+            [
+                [0, 0, 0],
+                [0, 1, 0],
+                [0, 0, 0],
+            ],
+        ])
+        res = torch_img_gradient.img_grad_dy(img)
+        ans = torch.FloatTensor([
+            [
+                [0, 1, 0],
+                [0, 0, 0],
+                [0, -1, 0],
+            ],
+        ])
+        self.assertTrue((res - ans).norm() < 1e-6)
+
 
 if __name__ == '__main__':
     unittest.main()
