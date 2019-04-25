75ee44246c50ba1c9d76030ed20402c0d2a270d8
Kirill Klimov <ra22341@ya.ru>
2019-04-25 15:32:10 +0300

    minor changes

---
+++ py3/rasterizer1/main_tmp.py
@@ -0,0 +1,14 @@
+import rasterizer_cpp
+
+
+def main():
+    print(rasterizer_cpp)
+    print(rasterizer_cpp.barycoords_from_2d_trianglef(
+        0, 0,
+        1, 0,
+        0, 1,
+        0.5, 0.5))
+
+
+if __name__ == '__main__':
+    main()
