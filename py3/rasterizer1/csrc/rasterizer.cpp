82b969083bd0beefc45d7f79e3a33ae89ef7db56
Kirill Klimov <ra22341@ya.ru>
2019-04-25 15:25:05 +0300

    minor changes

--- py3/rasterizer1/csrc/rasterizer.cpp
+++ py3/rasterizer1/csrc/rasterizer.cpp
@@ -10,5 +10,5 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
 //  m.def("forward", &lltm_forward, "LLTM forward");
 //  m.def("backward", &lltm_backward, "LLTM backward");
 //  m.def("foo", &foo, "Foo?");
-  m.def("barycoords_from_2d_trianglef", &barycoords_from_2d_trianglef, "Foo?");
+  m.def("barycoords_from_2d_trianglef", &Barycentric::barycoords_from_2d_trianglef, "Foo?");
 }
