7792a2a35f7c0cc173e637ebc68879d2711b059f
Kirill Klimov <ra22341@ya.ru>
2019-04-25 16:37:26 +0300

    minor changes

--- py3/rasterizer1/tests/test_barycentrics.py
+++ py3/rasterizer1/tests/test_barycentrics.py
@@ -13,6 +13,46 @@ class TestBarycentrics(unittest.TestCase):
             0.5, 0.5), dtype=np.float32)
         self.assertTrue(np.allclose(res, [0, 0.5, 0.5]))
 
+    def test_barycentrics_from_2d_vertices_02(self):
+        res = np.array(rasterizer_cpp.barycoords_from_2d_trianglef(
+            0, 0,
+            1, 0,
+            0, 1,
+            0, 0), dtype=np.float32)
+        self.assertTrue(np.allclose(res, [1, 0, 0]))
+
+    def test_barycentrics_from_2d_vertices_03(self):
+        res = np.array(rasterizer_cpp.barycoords_from_2d_trianglef(
+            0, 0,
+            1, 0,
+            0, 1,
+            1, 0), dtype=np.float32)
+        self.assertTrue(np.allclose(res, [0, 1, 0]))
+
+    def test_barycentrics_from_2d_vertices_04(self):
+        res = np.array(rasterizer_cpp.barycoords_from_2d_trianglef(
+            0, 0,
+            1, 0,
+            0, 1,
+            0, 1), dtype=np.float32)
+        self.assertTrue(np.allclose(res, [0, 0, 1]))
+
+    def test_barycentrics_from_2d_vertices_05(self):
+        res = np.array(rasterizer_cpp.barycoords_from_2d_trianglef(
+            0, 0,
+            1, 0,
+            0, 1,
+            0.5, 0), dtype=np.float32)
+        self.assertTrue(np.allclose(res, [0.5, 0.5, 0]))
+
+    def test_barycentrics_from_2d_vertices_06(self):
+        res = np.array(rasterizer_cpp.barycoords_from_2d_trianglef(
+            0, 0,
+            1, 0,
+            0, 1,
+            0, 0.5), dtype=np.float32)
+        self.assertTrue(np.allclose(res, [0.5, 0, 0.5]))
+
 
 if __name__ == '__main__':
     unittest.main()
