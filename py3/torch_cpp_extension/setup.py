41d6592f7d6a3bbee10115006a0ee201c3cbdce1
Kirill Klimov <ra22341@ya.ru>
2019-04-25 12:09:35 +0300

    minor changes

--- py3/torch_cpp_extension/setup.py
+++ py3/torch_cpp_extension/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 from torch.utils.cpp_extension import CppExtension, BuildExtension
 
 setup(name='lltm_cpp',
-      ext_modules=[CppExtension('lltm', ['lltm.cpp'])],
+      ext_modules=[CppExtension('lltm_cpp', ['lltm.cpp'])],
       cmdclass={'build_ext': BuildExtension})
