53f793205f40f9395d0a606187b582d437748a4b
Kirill Klimov <ra22341@ya.ru>
2019-05-23 14:41:26 +0300

    minor changes

--- py3/networkx_test/main.py
+++ py3/networkx_test/main.py
@@ -5,18 +5,9 @@ import matplotlib.pyplot as plt
 def main():
     g = nx.Graph()
     g.add_edge(0, 1)
-    g.add_edge(1, 2)
-    g.add_edge(2, 3)
-    g.add_edge(0, 3)
-    g.add_edge(0, 4)
-    g.add_edge(1, 4)
-    g.add_edge(2, 4)
-    g.add_edge(3, 4)
-
-    print(g.nodes())
-    print(g.edges())
-    print(list(g.adj[1]))
-    print(g[1])
+    g.add_edge(0, 2)
+    g.add_edge(3, 1)
+    g.add_edge(4, 2)
 
     print("Start search")
     bfs_res = nx.bfs_predecessors(g, 0)
@@ -27,12 +18,12 @@ def main():
     for x in bfs_res:
         print(x)
     print("Start search")
-    bfs_res = nx.bfs_tree(g, 0)
+    bfs_res = nx.bfs_tree(g, 0, depth_limit=1)
     for x in bfs_res:
         print(x)
 
-    # nx.draw(g)
-    # plt.show()
+    nx.draw(g)
+    plt.show()
 
 
 if __name__ == '__main__':
