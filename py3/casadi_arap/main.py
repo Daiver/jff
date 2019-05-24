dbc6450b3e7ce631a2f1a5c1afbf48429046850c
Kirill Klimov <ra22341@ya.ru>
2019-05-24 10:05:07 +0300

    minor changes

--- py3/casadi_arap/main.py
+++ py3/casadi_arap/main.py
@@ -3,17 +3,18 @@ import casadi
 from casadi import SX, Function
 
 
-def mk_point_to_point_residual(vertex, target):
+def point_to_point_residual(vertex, target):
     return vertex - target
 
 
-# def mk_
+def arap_residual(fan_center, fan_neighbours):
+    return (fan_neighbours - fan_center).reshape((-1, 1))
 
 
-def main():
+def test01():
     vertex = SX.sym("vertex", 3)
     target = SX.sym("target", 3)
-    p2p_func = Function("point2point", [vertex, target], [mk_point_to_point_residual(vertex, target)])
+    p2p_func = Function("point2point", [vertex, target], [point_to_point_residual(vertex, target)])
 
     n_vertices = 2
     vertices = SX.sym("vertices", 3, n_vertices)
@@ -32,18 +33,35 @@ def main():
     #     np.array([[0, 0, 0], [0, -1, 66]], dtype=np.float32).T,
     # ))
     variables = casadi.vertcat(casadi.reshape(vertices, -1, 1))
-    print(variables)
     jac = casadi.jacobian(residuals, variables)
-    print(jac)
     jac_f = Function("jac_f", [vertices, targets], [jac])
-    print(jac_f)
 
     num_jac_val = jac_f(
         np.array([[1, 0, 2], [4, -1, 5]], dtype=np.float32).T,
         np.array([[0, 0, 0], [0, -1, 66]], dtype=np.float32).T)
-    print(num_jac_val)
 
-    print(type(num_jac_val.toarray()))
+    print(variables)
+    print(jac)
+    print("jac.nnz()", jac.nnz())
+    print("jac.is_dense()", jac.is_dense())
+    print(jac_f)
+    print(num_jac_val)
+    print("num_jac_val.is_dense()", num_jac_val.is_dense())
+
+    print(type(num_jac_val.sparse()))
+
+
+def main():
+    # test01()
+    n_vertices = 5
+    vertices = SX.sym("vertices", 3, n_vertices)
+    fan_center = vertices[:, 0]
+    ring1 = vertices[:, 1:]
+
+    print(vertices)
+    print(fan_center)
+    print(ring1)
+    print(arap_residual(fan_center, ring1))
 
 
 if __name__ == '__main__':
