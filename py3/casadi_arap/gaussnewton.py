8c7da99f8d42d7d2e93a3bf3ac846c5e608a8ad6
Kirill Klimov <ra22341@ya.ru>
2019-05-24 14:09:03 +0300

    minor changes

--- py3/casadi_arap/gaussnewton.py
+++ py3/casadi_arap/gaussnewton.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.sparse.linalg
 
 
-def solve_least_squares(a_csc, residuals):
+def solve_least_squares(a_csc, residuals, dumping_factor):
     grad = a_csc.T @ residuals
     h = a_csc.T @ a_csc
 
@@ -10,25 +10,31 @@ def solve_least_squares(a_csc, residuals):
     # print(h.shape)
     # print(h.toarray())
     # print(np.where(~h.toarray().any(axis=1))[0])
-    # h += 1e-10 * scipy.sparse.eye(h.shape[0])
+    h += dumping_factor * scipy.sparse.eye(h.shape[0])
     return -scipy.sparse.linalg.spsolve(h, grad)
 
 
-def perform_gauss_newton(x0, compute_res_and_jac, n_iters=10):
+def perform_gauss_newton(x0, compute_res_and_jac, n_iters=10, dumping_factor=0.0):
+    threshold_err = 1e-20
+
     x = x0.copy()
     residuals, jac = compute_res_and_jac(x)
+    grad = jac.T @ residuals
+    grad_norm = np.abs(grad).sum()
     err = 0.5 * (residuals ** 2).sum()
 
-    print(f"E(x0) = {err}")
+    print(f"E(x0) = {err}, ||∇E(x0)|| = {grad_norm}")
     for iter_num in range(n_iters):
-        step = solve_least_squares(jac, residuals)
+        step = solve_least_squares(jac, residuals, dumping_factor)
         x += step
 
         residuals, jac = compute_res_and_jac(x)
+        grad = jac.T @ residuals
+        grad_norm = np.abs(grad).sum()
         err = 0.5 * (residuals ** 2).sum()
-        print(f"{iter_num + 1} / {n_iters} E(x) = {err}")
+        print(f"{iter_num + 1} / {n_iters} E(x) = {err}, ||∇E(x)|| = {grad_norm}")
 
-        if err < 1e-9:
+        if err < threshold_err:
             break
 
     return x
