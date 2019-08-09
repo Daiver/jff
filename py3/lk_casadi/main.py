ddae318ab133b3040aa39b9327657eef5da4e8aa
Kirill Klimov <ra22341@ya.ru>
2019-08-09 17:33:19 +0300

    minor chanegs

--- py3/lk_casadi/main.py
+++ py3/lk_casadi/main.py
@@ -15,18 +15,33 @@ def mk_images(canvas_size):
     return img1, img2
 
 
-def perform_lk(img1, img2, start_point):
+def perform_lk(img1, img2, start_point, patch_size=(9, 9)):
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-    point_mx = MX.sym("point", 2, 1)
     img_coords_x = np.arange(0, img1.shape[1]).astype(np.float32)
     img_coords_y = np.arange(0, img1.shape[0]).astype(np.float32)
 
     img1_flat = img1.ravel(order='F').astype(np.float32)
-    img1_interpolant = casadi.interpolant("img1_interpolant", "bspline", [img_coords_x, img_coords_y], img1_flat)
-    print(img1_interpolant(DM(np.array([[0, 0], [64, 64]]).T)))
+    img2_flat = img2.ravel(order='F').astype(np.float32)
 
+    img1_interpolant = casadi.interpolant("img1_interpolant", "bspline", [img_coords_x, img_coords_y], img1_flat)
+    img2_interpolant = casadi.interpolant("img1_interpolant", "bspline", [img_coords_x, img_coords_y], img2_flat)
+    #
+    # print(img1_interpolant(DM(np.array([[0, 0], [64, 64]]).T)))
+    # print(img2_interpolant(DM(np.array([[0, 0], [70, 64]]).T)))
+
+    patch_grid = np.zeros((patch_size[0] * patch_size[1], 2), dtype=np.float32)
+    counter = 0
+    for x in range(patch_size[1]):
+        for y in range(patch_size[0]):
+            patch_grid[counter] = (x, y)
+            counter += 1
+
+    patch_grid = DM(patch_grid.T)
+    print(img1_interpolant(patch_grid))
+
+    point_mx = MX.sym("point", 2, 1)
     #
     # xgrid = np.linspace(-5,5,11)
     # ygrid = np.linspace(-4,4,9)
