975a2410d14d4c6c94a10dc3803cb056971ab4d4
Daiver <ra22341@ya.ru>
2019-08-25 17:47:14 +0300

    tmp

--- py3/casadi_quadratic_fit/main.py
+++ py3/casadi_quadratic_fit/main.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 import casadi
 from casadi import SX, Function
 
+from pygauss_newton import gauss_newton
+
 
 def mk_quadratic(a, b, c):
     def inner_quadratic(x):
-        return a * x * x + b * x + c
+        # return a * x * x + b * x + c
+        return a * x
     return inner_quadratic
 
 
-def point_to_curve_residual(curve_function, current_x, target_point):
+def mk_point_to_curve_residual(curve_function, current_x, target_point):
     assert target_point.shape == (2, 1)
     y = curve_function(current_x)
     point = casadi.vcat((current_x, y))
@@ -32,70 +35,75 @@ def mk_unrolled_gauss_newton(residuals_func, jacobian_func, x0, n_iters, dumping
 
         step = casadi.inv(hessian) @ gradient
         x = x - step
+
     residuals = residuals_func(x)
+    loss = 0.5 * residuals.T @ residuals
     jacobian = jacobian_func(x)
-    return x, 0.5 * residuals.T @ residuals, jacobian.T @ residuals
+    hessian = jacobian.T @ residuals
+    return x, residuals, loss, hessian
 
 
 def main():
-    a, b, c = SX(1), SX(0), SX(0)
     a = SX.sym("a")
-    target = SX([2, 2])
+    b, c = SX(0), SX(0)
+    # target = SX([2, 2])
+    # target = SX([3, 2])
+    targets = [
+        [1, 0],
+        # [0, 0],
+        [0, 1],
+    ]
+    # targets = SX(targets)
     # target = SX([1, 1])
     quadratic_sx = mk_quadratic(a, b, c)
-    x_sx = SX.sym("x")
-    residuals = point_to_curve_residual(quadratic_sx, x_sx, target)
-    jacobian = casadi.jacobian(residuals, x_sx)
 
-    residuals_func = Function("residuals_func", [x_sx], [residuals])
-    jacobian_func = Function("jacobian_func", [x_sx], [jacobian])
+    unroll_dump_const = 0.1
+    x_sxs = SX.sym("x", len(targets))
+    residuals_to_point = []
+    unrolled_functions = []
+    for i, target in enumerate(targets):
+        x_sx = x_sxs[i]
+        point_to_curve_residual = mk_point_to_curve_residual(quadratic_sx, x_sx, SX(target))
+        point_to_curve_jacobian = casadi.jacobian(point_to_curve_residual, x_sx)
 
-    dump_const = 0.1
-    gauss_newton = mk_unrolled_gauss_newton(residuals_func, jacobian_func, x_sx, 10, dump_const)
-    gauss_newton_func = Function("gauss_newton_func", [x_sx, a], gauss_newton)
+        point_to_curve_residuals_func = Function(f"point_to_curve_residuals_func{i}", [x_sx], [point_to_curve_residual])
+        point_to_curve_jacobian_func = Function(f"point_to_curve_jacobian_func{i}", [x_sx], [point_to_curve_jacobian])
 
-    print(casadi.jacobian(gauss_newton_func(x_sx, a)[0], a))
+        unrolled = mk_unrolled_gauss_newton(
+            point_to_curve_residuals_func, point_to_curve_jacobian_func, x_sx, 15, unroll_dump_const)
+        unrolled_func = Function("unrolled_func", [x_sx, a], unrolled)
+        unrolled_functions.append(unrolled_func)
+        residual_to_point = unrolled[1]
+        residuals_to_point.append(residual_to_point)
 
-    variables = [0]
-    print(gauss_newton_func(variables, [1]))
+    residuals_to_point = casadi.vertcat(*residuals_to_point)
+    residuals_to_point_func = Function("residuals_to_point_func", [x_sxs, a], [residuals_to_point])
 
+    point_to_curve_jacobian = casadi.jacobian(residuals_to_point_func(x_sxs, a), a)
+    point_to_curve_jacobian_func = Function("residuals_to_point_jac", [x_sxs, a], [point_to_curve_jacobian])
 
-def main1():
-    a, b, c = SX(1), SX(0), SX(0)
-    target = SX([2, 2])
-    # target = SX([1, 1])
-    quadratic_sx = mk_quadratic(a, b, c)
-    x_sx = SX.sym("x")
-    residuals = point_to_curve_residual(quadratic_sx, x_sx, target)
-    jacobian = casadi.jacobian(residuals, x_sx)
-    hessian, gradient = casadi.hessian(0.5 * residuals.T @ residuals, x_sx)
-    # print(hessian)
+    print(residuals_to_point_func)
 
-    residuals_func = Function("residuals_func", [x_sx], [residuals])
-    jacobian_func = Function("jacobian_func", [x_sx], [jacobian])
-    # hessian_func = Function("hessian_func", [x_sx], [hessian])
+    dump_const = 0.0001
 
-    dump_const = 0.1
-    gauss_newton = mk_unrolled_gauss_newton(residuals_func, jacobian_func, x_sx, 10, dump_const)
-    gauss_newton_func = Function("gauss_newton_func", [x_sx], [gauss_newton])
+    vars = np.array([[1.0]])
+    xs = np.array([0.0] * len(targets))
+    for i in range(20):
+        residuals_val = residuals_to_point_func(xs, vars)
+        jacobian_val = point_to_curve_jacobian_func(xs, vars)
 
-    variables = [0]
-    for i in range(10):
-        residuals_val = residuals_func(variables).toarray()
-        jacobian_val = jacobian_func(variables).toarray()
-        loss_val = residuals_val.T.dot(residuals_val)
-
+        loss_val = 0.5 * residuals_val.T @ residuals_val
         gradient_val = jacobian_val.T @ residuals_val
         hessian_val = jacobian_val.T @ jacobian_val
-        # hessian_val = hessian_func(variables)
         hessian_val += dump_const * np.eye(hessian_val.shape[0])
-        step = np.linalg.solve(hessian_val, gradient_val)
 
-        print(f"{i} loss {loss_val} var = {variables}, \grad_norm = {np.linalg.norm(gradient_val)}")
-        variables -= step
+        step = -np.linalg.solve(hessian_val, gradient_val)
 
-    print(variables)
-    print(gauss_newton_func(variables))
+        print(f"{i}, loss = {loss_val}, grad_norm = {np.linalg.norm(gradient_val)}")
+        vars += step
+        for x, unrolled_func in zip(xs, unrolled_functions):
+            print(vars, unrolled_func(x, vars)[0])
+    print(vars)
 
 
 if __name__ == '__main__':
