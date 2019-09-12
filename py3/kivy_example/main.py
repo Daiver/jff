46e2e053e3e05a7fa6c3fa178afa8b7a7b8d870a
Kirill Klimov <ra22341@ya.ru>
2019-09-12 13:44:42 +0300

    minor changes

--- py3/kivy_example/main.py
+++ py3/kivy_example/main.py
@@ -1,5 +1,8 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.progressbar import ProgressBar
+from kivy.uix.button import Button
+from functools import partial
 
 
 class PongGame(Widget):
@@ -7,8 +10,25 @@ class PongGame(Widget):
 
 
 class PongApp(App):
+ 
+    def disable(self, instance, *args):
+ 
+        instance.disabled = True
+ 
+    def update(self, instance, *args):
+ 
+        instance.text = "I am Disabled!"
+ 
     def build(self):
-        return PongGame()
+ 
+        mybtn = Button(text="Click me to disable")
+ 
+        mybtn.bind(on_press=partial(self.disable, mybtn))
+ 
+        mybtn.bind(on_press=partial(self.update, mybtn))
+        pb = ProgressBar(max=1000) 
+        pb.value = 750
+        return pb
 
 
 if __name__ == '__main__':
