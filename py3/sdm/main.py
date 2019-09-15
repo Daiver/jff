878a554bf409b39bcc35cdbcfc784a861ca72fde
Daiver <ra22341@ya.ru>
2019-09-15 09:13:16 +0300

    1

---
+++ py3/sdm/main.py
@@ -0,0 +1,154 @@
+
+import argparse
+import torch
+import torch.utils.data
+import datetime
+from torch import nn, optim
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+
+
+torch.manual_seed(42)
+
+epochs = 10
+batch_size = 32
+device = "cuda:0"
+latent_size = 16
+hidden_size = 400
+log_interval = 1
+
+
+kwargs = {'num_workers': 8, 'pin_memory': True} 
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('data', train=True, download=True,
+                   transform=transforms.ToTensor()),
+    batch_size=batch_size, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('data', train=False, transform=transforms.ToTensor()),
+    batch_size=batch_size, shuffle=True, **kwargs)
+
+
+class VAE(nn.Module):
+    def __init__(self):
+        # activation = nn.ReLU()
+        activation = nn.ELU
+        super(VAE, self).__init__()
+        self.encoder_base = nn.Sequential(
+            nn.Linear(784, hidden_size),
+            activation(),
+            nn.Linear(hidden_size, hidden_size),
+            activation(),
+        )
+        self.fc21 = nn.Linear(hidden_size, latent_size)
+        self.fc22 = nn.Linear(hidden_size, latent_size)
+
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_size, hidden_size),
+            activation(),
+            nn.Linear(hidden_size, hidden_size),
+            activation(),
+            nn.Linear(hidden_size, 784),
+            nn.Sigmoid()
+        )
+
+    def encode(self, x):
+        h1 = self.encoder_base(x)
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+
+    def decode(self, z):
+        return self.decoder(z)
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+
+model = VAE().to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+
+def classes_to_one_hot(sample, n_classes=10):
+    res = torch.zeros((sample.size(0), n_classes))
+    for i, s in enumerate(sample):
+        res[i, s] = 1
+    return res
+
+
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return BCE + KLD
+
+
+def run_training_stage(epoch):
+    model.train()
+    train_loss = 0
+
+    start_time = datetime.datetime.now()
+    prefix = 'vanila'
+
+    for batch_idx, (data, labels) in enumerate(train_loader):
+        data = data.to(device)
+        # print(labels)
+
+        optimizer.zero_grad()
+        recon_batch, mu, logvar = model(data)
+        loss = loss_function(recon_batch, data, mu, logvar)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.item() / len(data)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+    torch.save(model, f'checkpoints/{prefix}_{str(start_time)}_{epoch}.pt')
+
+
+def run_testing_stage(epoch):
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for i, (data, labels) in enumerate(test_loader):
+            data = data.to(device)
+            labels = labels.to(device)
+            recon_batch, mu, logvar = model(data)
+            test_loss += loss_function(recon_batch, data, mu, logvar).item()
+            if i == 0:
+                n = min(data.size(0), 8)
+                comparison = torch.cat([data[:n],
+                                      recon_batch.view(batch_size, 1, 28, 28)[:n]])
+                save_image(comparison.cpu(),
+                         'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+
+
+if __name__ == "__main__":
+    for epoch in range(1, epochs + 1):
+        run_training_stage(epoch)
+        run_testing_stage(epoch)
+        with torch.no_grad():
+            sample = torch.randn(64, latent_size).to(device)
+            sample = model.decode(sample).cpu()
+            save_image(sample.view(64, 1, 28, 28),
+                       'results/sample_' + str(epoch) + '.png')
