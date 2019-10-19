794529f5bd78c2521cae507ba41db003da3c1694
Kirill Klimov <ra22341@ya.ru>
2019-10-19 17:21:23 +0300

    minor changes

---
+++ py3/pointnet_try01/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SpatialTransformer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.feature_extractor = nn.Sequential(
+
+        )
+
+    def forward(self, x):
+        assert False
+        return x
