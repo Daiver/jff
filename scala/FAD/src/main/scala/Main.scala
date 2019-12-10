185c77854c3bdfeff8fd14dae506d81651b19ee8
Daiver <ra22341@ya.ru>
2019-12-10 23:58:49 +0300

    Update Main.scala

--- scala/FAD/src/main/scala/Main.scala
+++ scala/FAD/src/main/scala/Main.scala
@@ -1,18 +1,31 @@
-class A(multiplier: Int){
-  def f(x: Int): Int = multiplier * x
-}
+//import scala.math.Numeric
 
 object Main extends App {
-  def foo(): Int = 13
-  def foo2(x: Int): Int = {
-    x*x
+
+  case class Fad(value: Float, der: Float)
+
+  trait FadIsNumeric extends Numeric[Fad] {
+    def plus(x: Fad, y: Fad): Fad = Fad(x.value + y.value, x.der + y.der)
+    def minus(x: Fad, y: Fad): Fad = ???
+    def times(x: Fad, y: Fad): Fad = Fad(x.value * y.value, x.value * y.der + x.der * y.value)
+    def negate(x: Fad): Fad = ???
+    def fromInt(x: Int): Fad = ???
+    def parseString(str: String): Option[Fad] = ???
+    def toInt(x: Fad): Int = ???
+    def toLong(x: Fad): Long = ???
+    def toFloat(x: Fad): Float = ???
+    def toDouble(x: Fad): Double = ???
   }
-  val f = (x: Int) => x + 1
-  val a = new A(7)
-  println(f(13))
-  println(foo())
-  println(foo2(13))
-  println(a.f(7))
+  implicit object FadIsNumeric extends FadIsNumeric with Ordering[Fad] {
+    override def compare(x: Fad, y: Fad): Int = ???
+  }
+
+  def square[T](x: T)(implicit num: Numeric[T]): T = {
+    import num._
+    x * x
+  }
+  val x = new Fad(3, 1)
+  println(square(x))
   println("Hello World!")
 
 }
