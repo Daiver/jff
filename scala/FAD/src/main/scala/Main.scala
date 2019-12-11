22b75da294de9321d6ffedec4d25c0b65ffa6be8
Daiver <ra22341@ya.ru>
2019-12-11 23:34:41 +0300

    Update Main.scala

--- scala/FAD/src/main/scala/Main.scala
+++ scala/FAD/src/main/scala/Main.scala
@@ -6,10 +6,10 @@ object Main extends App {
 
   trait FadIsNumeric extends Numeric[Fad] {
     def plus(x: Fad, y: Fad): Fad = Fad(x.value + y.value, x.der + y.der)
-    def minus(x: Fad, y: Fad): Fad = ???
+    def minus(x: Fad, y: Fad): Fad = Fad(x.value - y.value, x.der - y.der)
     def times(x: Fad, y: Fad): Fad = Fad(x.value * y.value, x.value * y.der + x.der * y.value)
-    def negate(x: Fad): Fad = ???
-    def fromInt(x: Int): Fad = ???
+    def negate(x: Fad): Fad = Fad(-x.value, -x.der)
+    def fromInt(x: Int): Fad = Fad(x, 0)
     def parseString(str: String): Option[Fad] = ???
     def toInt(x: Fad): Int = ???
     def toLong(x: Fad): Long = ???
