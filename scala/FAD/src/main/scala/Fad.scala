f8af4cf47d717c8a833f9f57ac42cffb056d59c5
Daiver <ra22341@ya.ru>
2019-12-17 18:46:23 +0300

    Minor changes

--- scala/FAD/src/main/scala/Fad.scala
+++ scala/FAD/src/main/scala/Fad.scala
@@ -1,9 +1,9 @@
 //case class Fad()
 
-case class Fad(value: Float, der: Float)
+case class Fad(value: Float, der: Float = 0.0f)
 object Fad {
 
-  trait FadIsNumeric extends Numeric[Fad] {
+  trait FadIsFractional extends Fractional[Fad] {
     def plus(x: Fad, y: Fad): Fad = Fad(x.value + y.value, x.der + y.der)
 
     def minus(x: Fad, y: Fad): Fad = Fad(x.value - y.value, x.der - y.der)
@@ -11,6 +11,9 @@ object Fad {
     def times(x: Fad, y: Fad): Fad = Fad(x.value * y.value, x.value * y.der + x.der * y.value)
 
     def negate(x: Fad): Fad = Fad(-x.value, -x.der)
+
+    def div(x: Fad, y: Fad): Fad =
+      Fad(x.value / y.value, (y.value * x.der - x.value * y.der) / (y.value * y.value))
 
     def fromInt(x: Int): Fad = Fad(x, 0)
 
@@ -25,8 +28,8 @@ object Fad {
     def toDouble(x: Fad): Double = ???
   }
 
-  implicit object FadIsNumeric extends FadIsNumeric with Ordering[Fad] {
+  implicit object FadIsFractional extends FadIsFractional with Ordering[Fad] {
     override def compare(x: Fad, y: Fad): Int = ???
   }
-
+  implicit def floatToFad(x: Float): Fad = Fad(x, 0)
 }
--- scala/FAD/src/test/scala/FADTests.scala
+++ scala/FAD/src/test/scala/FADTests.scala
@@ -1,13 +1,83 @@
 import org.scalatest.FunSuite
+import Float._
 
 class FADTests extends FunSuite {
-  test("Fad01") {
-    val x = Fad(1, 1)
-    assert(3*3*3 === 27)
+  import Fad.FadIsFractional._
+
+  def isAlmostEqual(a: Float, b: Float, eps: Float = 1e-9f): Boolean = (a - b).abs < eps
+  def isAlmostEqualFad(a: Fad, b: Fad, eps: Float = 1e-9f): Boolean =
+    isAlmostEqual(a.value, b.value, eps) && isAlmostEqual(a.der, b.der, eps)
+
+  test("isAlmostEqual01") {
+    assert(isAlmostEqual(5f, 5.01f, 0.1f))
+    assert(!isAlmostEqual(5f, 5.2f, 0.1f))
   }
 
-  test("Fad02") {
+  test("isAlmostEqualFad01") {
+    assert(isAlmostEqualFad(Fad(1, 2), Fad(1.01f, 1.96f), 0.1f))
+    assert(!isAlmostEqualFad(Fad(0, 2), Fad(1.01f, 1.96f), 0.1f))
+  }
+
+  test("Fad01") {
+    val x = Fad(1, 1)
+    assert(x.value === 1.0f)
+    assert(x.der === 1.0f)
+  }
+
+  test("Fad add 01") {
+    val res1 = Fad(-1.0f) + Fad(2, 1)
+    assert(isAlmostEqualFad(res1, Fad(1, 1)))
+  }
+
+  test("Fad add 02") {
+    val res1 = Fad(2, 5) + Fad(1, 3)
+    assert(isAlmostEqualFad(res1, Fad(3, 8)))
+  }
+
+  test("Fad minus 01") {
+    val res1 = Fad(2, 5) - Fad(1, 3)
+    assert(isAlmostEqualFad(res1, Fad(1, 2)))
+  }
+
+  test("Fad times 01") {
     val x = Fad(0, 0)
-    assert(3*3*3 === 27)
+    val y: Fad = 2.0f
+    val res1 = Fad(-1.0f) * x
+    val res2 = Fad(-1.0f) * y
+    assert(isAlmostEqualFad(res1, Fad(0, 0)))
+    assert(isAlmostEqualFad(res2, Fad(-2, 0)))
+  }
+
+  test("Fad times 02") {
+    val res1 = Fad(3.0f) * Fad(4, 5)
+    val res2 = Fad(-2.0f, 1) * Fad(3, 1)
+    assert(isAlmostEqualFad(res1, Fad(12, 15)))
+    assert(isAlmostEqualFad(res2, Fad(-6, 1)))
+  }
+
+  test("Fad negate 01") {
+    val res1 = -Fad(2, -5)
+    assert(isAlmostEqualFad(res1, Fad(-2, 5)))
+  }
+
+  test("Fad division 01") {
+    val x = Fad(5)
+    val y = Fad(2)
+    val res = x / y
+    assert(isAlmostEqualFad(res, Fad(2.5f, 0)))
+  }
+
+  test("Fad division 02") {
+    val x = Fad(5, 3)
+    val y = Fad(2)
+    val res = x / y
+    assert(isAlmostEqualFad(res, Fad(2.5f, 1.5f)))
+  }
+
+  test("Fad division 03") {
+    val x = Fad(5, 3)
+    val y = Fad(2, 1)
+    val res = x / y
+    assert(isAlmostEqualFad(res, Fad(2.5f, 1.5f)))
   }
 }
