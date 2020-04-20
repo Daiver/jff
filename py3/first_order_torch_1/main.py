e3d17ecf84762fbed249318065cf016eb0d7b624
Kirill Klimov <ra22341@ya.ru>
2020-04-20 23:18:36 +0300

    minor changes

--- py3/first_order_torch_1/main.py
+++ py3/first_order_torch_1/main.py
@@ -4,13 +4,12 @@ import numpy as np
 import cv2
 import torch
 from torch.utils.data import DataLoader
-import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import torchvision
 
 import np_draw_tools
 
+from models import KpPredictor
 
 seed = 42
 random.seed(seed)
@@ -20,16 +19,16 @@ torch.manual_seed(seed)
 
 def draw_background(canvas_size: Union[List[int], Tuple[int, int]]):
     canvas = np.zeros((canvas_size[0], canvas_size[1], 3), dtype=np.float32)
-    # scale = canvas_size[0] / 64.0 * 1.5
+    scale = canvas_size[0] / 64.0 * 0.5
     # cv2.putText(
     #     canvas, "A",
     #     (canvas_size[0] // 2, canvas_size[1] // 2),
     #     cv2.FONT_HERSHEY_COMPLEX, scale, (0.5, 0.5, 0.5))
     #
-    # cv2.putText(
-    #     canvas, "B",
-    #     (canvas_size[0] // 2 - canvas_size[0] // 3, canvas_size[1] // 2 + canvas_size[1] // 4),
-    #     cv2.FONT_HERSHEY_COMPLEX, scale, (0.0, 0.7, 0.5))
+    cv2.putText(
+        canvas, "B",
+        (canvas_size[0] // 2 - canvas_size[0] // 3, canvas_size[1] // 2 + canvas_size[1] // 4),
+        cv2.FONT_HERSHEY_COMPLEX, scale, (0.0, 0.7, 0.5))
     return canvas
 
 
@@ -47,7 +46,7 @@ def draw_fig1_on_background(background: np.ndarray, pos) -> np.ndarray:
 
 
 def mk_translation_sequence(background: np.ndarray, n_samples: int, samples_maker) -> np.ndarray:
-    random_translations = np.random.uniform(-10, 10, size=(n_samples, 2))
+    random_translations = np.random.uniform(-20, 20, size=(n_samples, 2))
     return np.array([
         samples_maker(background, pos)
         for pos in random_translations
@@ -67,65 +66,9 @@ def torch_translate_images(images, translations, device):
     return res
 
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, stride: int = 1):
-        assert stride == 1 or stride == 2
-        super().__init__()
-        self.strum = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=stride),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(),
-            nn.Conv2d(out_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(),
-        )
-
-        if out_channels != in_channels or stride != 1:
-            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride)
-        else:
-            self.shortcut = None
-
-    def forward(self, x):
-        z = self.strum(x)
-        if self.shortcut is not None:
-            x = self.shortcut(x)
-
-        return x + z
-
-
-class Model(nn.Module):
-    def __init__(self):
-        super().__init__()
-        n_feature_channels = 16
-        self.first_conv = nn.Sequential(
-            nn.Conv2d(3, n_feature_channels, kernel_size=7, padding=3, stride=2),
-            nn.BatchNorm2d(n_feature_channels),
-            nn.LeakyReLU()
-        )
-        self.block1 = ResidualBlock(in_channels=n_feature_channels, out_channels=n_feature_channels, stride=2)
-        self.block2 = ResidualBlock(in_channels=n_feature_channels, out_channels=n_feature_channels, stride=2)
-        self.block3 = ResidualBlock(in_channels=n_feature_channels, out_channels=n_feature_channels, stride=2)
-
-        n_fc_in = n_feature_channels * 4 * 4
-        self.fc = nn.Linear(n_fc_in, 2)
-        self.fc.weight.data.zero_()
-        self.fc.bias.data.zero_()
-
-    def forward(self, x):
-        x = self.first_conv(x)  # 64 -> 32
-        x = self.block1(x)      # 32 -> 16
-        x = self.block2(x)      # 16 -> 8
-        x = self.block3(x)      # 8  -> 4
-
-        x = x.view(x.shape[0], -1)
-        x = self.fc(x)
-
-        return x
-
-
 def main():
     canvas_size = (64, 64)
-    n_samples = 200
+    n_samples = 2048
     device = "cuda"
     batch_size = 32
     n_epochs = 100
@@ -133,25 +76,25 @@ def main():
 
     background = draw_background(canvas_size)
     fig1 = draw_fig1_on_background(background, [10, 10])
-    figs = mk_translation_sequence(background, n_samples, draw_fig1_on_background)
+    train_figs = mk_translation_sequence(background, n_samples, draw_fig1_on_background)
 
-    grid = np_draw_tools.make_grid(figs[:50], 4)
+    grid = np_draw_tools.make_grid(train_figs[:50], 4)
 
     cv2.imshow("back", background)
     cv2.imshow("fig1", grid)
     cv2.waitKey(100)
 
-    dataset = torch.from_numpy(figs).permute([0, 3, 1, 2])
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    train_dataset = torch.from_numpy(train_figs).permute([0, 3, 1, 2])
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 
-    model = Model().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=lr)
+    kp_predictor = KpPredictor().to(device)
+    optimizer = optim.Adam(kp_predictor.parameters(), lr=lr)
 
     for epoch_ind in range(n_epochs):
         losses = []
-        for batch_ind, batch in enumerate(dataloader):
+        for batch_ind, batch in enumerate(train_dataloader):
             batch = batch.to(device)
-            translations = model(batch)
+            translations = kp_predictor(batch)
 
             n_images = batch.shape[0]
             assert n_images % 2 == 0
@@ -175,8 +118,11 @@ def main():
 
         print(f"{epoch_ind + 1}/{n_epochs} loss: {np.mean(losses)}")
 
-    images = dataset[:20].to(device)
-    translations = model(images)
+    test_figs = mk_translation_sequence(background, n_samples, draw_fig1_on_background)
+    test_dataset = torch.from_numpy(test_figs).permute([0, 3, 1, 2])
+
+    images = test_dataset[:20].to(device)
+    translations = kp_predictor(images)
 
     n_images = images.shape[0]
     assert n_images % 2 == 0
---
+++ py3/first_order_torch_1/models.py
@@ -0,0 +1,122 @@
+from torch import nn as nn
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 1):
+        assert stride == 1 or stride == 2
+        super().__init__()
+        self.strum = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=stride),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(),
+            nn.Conv2d(out_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(),
+        )
+
+        if out_channels != in_channels or stride != 1:
+            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride)
+        else:
+            self.shortcut = None
+
+        for x in self.modules():
+            if isinstance(x, nn.BatchNorm2d):
+                x.bias.data.zero_()
+                x.weight.data.zero_()
+
+    def forward(self, x):
+        z = self.strum(x)
+        if self.shortcut is not None:
+            x = self.shortcut(x)
+
+        return x + z
+
+
+class LinkNetBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, scale: int = 1):
+        super().__init__()
+        assert scale == 1 or scale == 2
+        super().__init__()
+        self.strum = nn.Sequential(
+            nn.UpsamplingBilinear2d(scale_factor=scale),
+            nn.Conv2d(in_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(),
+            nn.Conv2d(out_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(),
+        )
+
+        if out_channels != in_channels or scale != 1:
+            self.shortcut = nn.Sequential(
+                nn.UpsamplingBilinear2d(scale_factor=scale),
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=1)
+            )
+        else:
+            self.shortcut = None
+
+    def forward(self, x):
+        z = self.strum(x)
+        if self.shortcut is not None:
+            x = self.shortcut(x)
+
+        return x + z
+
+
+class KpPredictor(nn.Module):
+    def __init__(self):
+        super().__init__()
+        n_feature_channels = 32
+        self.first_conv = nn.Sequential(
+            nn.Conv2d(3, n_feature_channels, kernel_size=7, padding=3, stride=1),
+            nn.BatchNorm2d(n_feature_channels),
+            nn.LeakyReLU()
+        )
+        self.block1 = ResidualBlock(in_channels=n_feature_channels, out_channels=n_feature_channels, stride=2)
+        self.block2 = ResidualBlock(in_channels=n_feature_channels, out_channels=n_feature_channels, stride=2)
+        self.block3 = ResidualBlock(in_channels=n_feature_channels, out_channels=n_feature_channels, stride=2)
+        self.block4 = ResidualBlock(in_channels=n_feature_channels, out_channels=n_feature_channels, stride=2)
+
+        n_fc_in = n_feature_channels * 4 * 4
+        self.fc = nn.Linear(n_fc_in, 2)
+        self.fc.weight.data.zero_()
+        self.fc.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.first_conv(x)  # 64 -> 64
+        x = self.block1(x)      # 64 -> 32
+        x = self.block2(x)      # 32 -> 16
+        x = self.block3(x)      # 16 -> 8
+        x = self.block4(x)      # 8  -> 4
+
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+
+        return x
+
+
+class Hourglass(nn.Module):
+    def __init__(self):
+        super().__init__()
+        n_feature_channels = 32
+        self.first_conv = nn.Sequential(
+            nn.Conv2d(3, n_feature_channels, kernel_size=7, padding=3, stride=1),
+            nn.BatchNorm2d(n_feature_channels),
+            nn.LeakyReLU()
+        )
+        self.down_block1 = ResidualBlock(in_channels=n_feature_channels, out_channels=n_feature_channels, stride=2)
+        self.down_block2 = ResidualBlock(in_channels=n_feature_channels, out_channels=n_feature_channels, stride=2)
+        self.down_block3 = ResidualBlock(in_channels=n_feature_channels, out_channels=n_feature_channels, stride=2)
+        self.down_block4 = ResidualBlock(in_channels=n_feature_channels, out_channels=n_feature_channels, stride=2)
+
+    def forward(self, x):
+        x = self.first_conv(x)  # 64 -> 64
+        x = self.block1(x)      # 64 -> 32
+        x = self.block2(x)      # 32 -> 16
+        x = self.block3(x)      # 16 -> 8
+        x = self.block4(x)      # 8  -> 4
+
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+
+        return x
