7a7fe3907192a82f663d22deb0ffcfe7d443e2e4
Kirill Klimov <ra22341@ya.ru>
2020-05-26 21:25:31 +0300

    minor changes

--- py3/torch_go_with_the_flow/tests/test_torch_tools.py
+++ py3/torch_go_with_the_flow/tests/test_torch_tools.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-from torch_tools import screen_to_norm
+from torch_tools import screen_to_norm, screen_to_norm_batch
 
 
 class TestTorchTools(unittest.TestCase):
@@ -25,5 +25,32 @@ class TestTorchTools(unittest.TestCase):
             [1, 1],
             [0, 0],
             [1, -1]
+        ], dtype=np.float32))
+        self.assertTrue(torch.allclose(ans, res))
+
+    def test_screen_to_norm_batch01(self):
+        width = 10
+        height = 5
+        coords = torch.from_numpy(np.array([
+            [
+                [0, 0],
+                [9, 4],
+            ],
+            [
+                [4.5, 2],
+                [9, 0]
+            ],
+        ], dtype=np.float32))
+
+        res = screen_to_norm_batch(coords, width, height, align_corners=True)
+        ans = torch.from_numpy(np.array([
+            [
+                [-1, -1],
+                [1, 1],
+            ],
+            [
+                [0, 0],
+                [1, -1]
+            ],
         ], dtype=np.float32))
         self.assertTrue(torch.allclose(ans, res))
--- py3/torch_go_with_the_flow/torch_tools.py
+++ py3/torch_go_with_the_flow/torch_tools.py
@@ -12,3 +12,17 @@ def screen_to_norm(coords: torch.Tensor, width: int, height: int, align_corners:
     res[:, 1] *= 2.0 / float(height - 1)
     res.sub_(1)
     return res
+
+
+def screen_to_norm_batch(coords: torch.Tensor, width: int, height: int, align_corners: bool = True):
+    assert align_corners
+    assert coords.dim() == 3
+    assert coords.shape[2] == 2
+    assert width > 0 and height > 0
+
+    res = coords.clone()
+    res[:, :, 0] *= 2.0 / float(width - 1)
+    res[:, :, 1] *= 2.0 / float(height - 1)
+    res.sub_(1)
+    return res
+
