8b37a66491215fe8cd19c4a2f0c665218e88cbd7
Kirill Klimov <ra22341@ya.ru>
2020-08-26 00:10:31 +0300

    idgf

--- py3/random_def_grad_shit1/main.py
+++ py3/random_def_grad_shit1/main.py
@@ -19,10 +19,28 @@ def deformation_gradient2d(triangle_vertices: SX, old_triangle_vertices: SX) -> 
     return new_shape_mat @ casadi.inv(old_shape_mat)
 
 
+def passive_energy_for_triangle(
+        triangle_rotation: SX,
+        triangle_vertices: SX,
+        old_triangle_vertices: SX,
+        triangle_area: float,
+        weight_corotation: float,
+        weight_volume_preserve: float
+) -> SX:
+    assert triangle_rotation.size1() == 2
+    assert triangle_rotation.size2() == 2
+    def_grad = deformation_gradient2d(triangle_vertices, old_triangle_vertices)
+    corotated_energy = weight_corotation * casadi.norm_2((def_grad - triangle_rotation).reshape((-1, 1)))
+    volume_preserve_energy = weight_volume_preserve * (casadi.det(def_grad) - 1)**2
+    return triangle_area * (corotated_energy + volume_preserve_energy)
+
+
 def main():
     vertices = SX.sym("v", 2, 3)
+    rotation = SX.sym("r", 2, 2)
     old_vertices = DM([1, 2, 3, 5, 8, 4]).reshape((2, 3))
-    res = deformation_gradient2d(vertices, old_vertices)
+
+    res = passive_energy_for_triangle(rotation, vertices, old_vertices, 1, 1, 3)
     print(res.shape)
     print(res)
 
