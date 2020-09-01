91ccd2f78fe7efa0d9b5e49a259a7422823360c2
Kirill Klimov <ra22341@ya.ru>
2020-09-01 23:30:34 +0300

    useless

--- py3/random_def_grad_shit1/main.py
+++ py3/random_def_grad_shit1/main.py
@@ -19,7 +19,27 @@ def deformation_gradient2d(triangle_vertices: SX, old_triangle_vertices: SX) -> 
     return new_shape_mat @ casadi.inv(old_shape_mat)
 
 
-def activation_matrix_from_activations(muscle_activations: SX) -> SX:
+def triangle_vertices_from_vertices(vertices: SX, triangle_inds: [int]) -> SX:
+    assert len(triangle_inds) == 3
+    return casadi.horzcat(vertices[:, triangle_inds[0]], vertices[:, triangle_inds[1]], vertices[:, triangle_inds[2]])
+
+
+def deformation_gradients2d(
+        vertices: SX,
+        old_vertices: SX,
+        triangle_vertex_indices: [[int]]
+) -> SX:
+    assert vertices.shape == old_vertices.shape
+    assert vertices.size1() == 2
+    res = []
+    for triangle_inds in triangle_vertex_indices:
+        triangle_vertices = triangle_vertices_from_vertices(vertices, triangle_inds)
+        old_triangle_vertices = triangle_vertices_from_vertices(old_vertices, triangle_inds)
+        res.append(deformation_gradient2d(triangle_vertices, old_triangle_vertices))
+    return casadi.horzcat(*res)
+
+
+def activation_matrix2d_from_activations(muscle_activations: SX) -> SX:
     res = casadi.horzcat(muscle_activations[0], 0, 0, muscle_activations[1])
     return res.reshape((2, 2))
 
@@ -52,16 +72,68 @@ def active_energy_for_triangle(
     return triangle_area * (corotated_energy + volume_preserve_energy)
 
 
+def energy_for_triangle_mesh(
+        vertices_positions: SX,
+        old_vertices_positions: SX,
+        rotations_per_passive_triangle: SX,
+        rotations_per_active_triangle: SX,
+        activations: SX,
+        passive_triangle_vertex_indices: [[int]],
+        active_triangle_vertex_indices: [[int]],
+        weight_corotation: float,
+        weight_volume_preserve: float
+) -> SX:
+    passive_def_grads = deformation_gradients2d(
+        vertices_positions,
+        old_vertices_positions,
+        passive_triangle_vertex_indices
+    )
+    active_def_grads = deformation_gradients2d(
+        vertices_positions,
+        old_vertices_positions,
+        active_triangle_vertex_indices
+    )
+
+    res = 0
+    for tri_ind, tri_vertex_inds in enumerate(passive_triangle_vertex_indices):
+        rotation = rotations_per_passive_triangle[:, 2 * tri_ind: 2 * (tri_ind + 1)]
+        def_grad = passive_def_grads[:, 2 * tri_ind: 2 * (tri_ind + 1)]
+        res += passive_energy_for_triangle(
+            rotation, def_grad,
+            triangle_area=1,
+            weight_corotation=weight_corotation, weight_volume_preserve=weight_volume_preserve
+        )
+
+    assert activations.size1() == 2
+    for tri_ind, tri_vertex_inds in enumerate(active_triangle_vertex_indices):
+        rotation = rotations_per_active_triangle[:, 2 * tri_ind: 2 * (tri_ind + 1)]
+        def_grad = active_def_grads[:, 2 * tri_ind: 2 * (tri_ind + 1)]
+        local_activations = activations[:, tri_ind]
+        target_grad = activation_matrix2d_from_activations(local_activations)
+        res += active_energy_for_triangle(
+            rotation, def_grad, target_grad=target_grad,
+            triangle_area=1,
+            weight_corotation=weight_corotation, weight_volume_preserve=weight_volume_preserve
+        )
+
+    return res
+
+
 def main():
     vertices = SX.sym("v", 2, 3)
     rotation = SX.sym("r", 2, 2)
     activations = SX.sym("a", 2)
     old_vertices = DM([1, 2, 3, 5, 8, 4]).reshape((2, 3))
 
-    def_grad = deformation_gradient2d(vertices, old_vertices)
-    target_grad = activation_matrix_from_activations(activations)
-    # res = passive_energy_for_triangle(rotation, def_grad, 1, 1, 3)
-    res = active_energy_for_triangle(rotation, def_grad, target_grad, 1, 1, 3)
+    res = energy_for_triangle_mesh(
+        vertices_positions=vertices, old_vertices_positions=old_vertices,
+        rotations_per_passive_triangle=rotation,
+        rotations_per_active_triangle=rotation,
+        activations=activations,
+        passive_triangle_vertex_indices=[[0, 1, 2]],
+        active_triangle_vertex_indices=[[0, 1, 2]],
+        weight_corotation=1, weight_volume_preserve=1
+    )
     print(res)
 
 
