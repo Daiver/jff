34961b127eea5d45c5119078cee1146b1eb1cb83
Daiver <ra22341@ya.ru>
2020-11-07 14:25:34 +0300

    yep

--- py3/torch_motion_retarget_autoencoder/main.py
+++ py3/torch_motion_retarget_autoencoder/main.py
@@ -60,40 +60,66 @@ def main():
         torch.from_numpy(x).float().permute(2, 0, 1) / 255.0
         for x in rect_set
     ]
+
+    train_circle_set = [
+        torch.from_numpy(x).float().permute(2, 0, 1) / 255.0
+        for x in circle_set
+    ]
+
     print(f"N samples {len(train_rect_set)}")
     train_rect_loader = DataLoader(train_rect_set, batch_size=batch_size, shuffle=True, drop_last=True)
-    val_rect_loader = DataLoader(train_rect_set, batch_size=batch_size, shuffle=False)
+    val_rect_loader = DataLoader(train_rect_set, batch_size=batch_size * 4, shuffle=False)
+
+    train_circle_loader = DataLoader(train_circle_set, batch_size=batch_size, shuffle=True, drop_last=True)
 
     encoder = Encoder().to(device)
     decoder_rect = Decoder().to(device)
-    optimizer = optim.Adam(list(encoder.parameters()) + list(decoder_rect.parameters()), lr=1e-4)
+    decoder_circle = Decoder().to(device)
+    optimizer = optim.Adam(list(encoder.parameters()) + list(decoder_rect.parameters()) + list(decoder_circle.parameters()), lr=1e-4)
     # criterion = nn.MSELoss()
     # criterion = nn.L1Loss()
     criterion = nn.BCELoss()
     for epoch_ind in range(epochs):
         losses = []
+        losses_rect = []
+        losses_circle = []
+
         encoder.train()
         decoder_rect.train()
-        for sample_rect in train_rect_loader:
+        decoder_circle.train()
+
+        for sample_rect, sample_circle in zip(train_rect_loader, train_circle_loader):
+
             sample_rect = sample_rect.to(device)
             pred_rect = encoder(sample_rect)
             pred_rect = decoder_rect(pred_rect)
             loss_rect = criterion(pred_rect, sample_rect)
 
-            loss = loss_rect
+            sample_circle = sample_circle.to(device)
+            pred_circle = encoder(sample_circle)
+            pred_circle = decoder_circle(pred_circle)
+            loss_circle = criterion(pred_circle, sample_circle)
+
+            loss = loss_rect + loss_circle
 
             losses.append(loss.item())
+            losses_rect.append(loss_rect.item())
+            losses_circle.append(loss_circle.item())
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
         encoder.eval()
         decoder_rect.eval()
-        print(f"{epoch_ind + 1} / {epochs} loss {np.mean(losses)}")
+        decoder_circle.eval()
+
+        print(f"{epoch_ind + 1} / {epochs} loss {np.mean(losses)} loss rect {np.mean(losses_rect)} loss circle {np.mean(losses_circle)}")
         for sample_rect in val_rect_loader:
             sample_rect = sample_rect.to(device)
             pred_rect = encoder(sample_rect)
-            pred_rect = decoder_rect(pred_rect)
+            # pred_rect = decoder_rect(pred_rect)
+            pred_rect = decoder_circle(pred_rect)
             pred_rect = (pred_rect.detach().permute(0, 2, 3, 1).cpu().numpy() * 255).astype(np.uint8)
             sample_rect = (sample_rect.detach().permute(0, 2, 3, 1).cpu().numpy() * 255).astype(np.uint8)
 
@@ -101,7 +127,7 @@ def main():
             for p, s in zip(pred_rect, sample_rect):
                 to_show.append(p)
                 to_show.append(s)
-            to_show = np_draw_tools.make_grid(to_show[:20], 4)
+            to_show = np_draw_tools.make_grid(to_show[:32], 4)
             cv2.imshow("", to_show)
             cv2.waitKey(100)
             break
