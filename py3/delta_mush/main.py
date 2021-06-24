9be21813fde4cd4636a98b1a450a91294dbd814e
Kirill Klimov <ra22341@ya.ru>
2021-06-24 10:03:02 +0300

    probably implemented delta mush

--- py3/delta_mush/main.py
+++ py3/delta_mush/main.py
@@ -1,6 +1,7 @@
 from typing import List
 import numpy as np
 import geom_tools
+from copy import deepcopy
 
 
 def laplacian_smoothing_one_step(
@@ -14,7 +15,7 @@ def laplacian_smoothing_one_step(
     assert vertices.ndim == 2 and vertices.shape[1] == 3
     res = vertices.copy()
     for vertex_ind in range(n_vertices):
-        average_vertex = np.zeros(3, dtype=np.float)
+        average_vertex = np.zeros(3, dtype=np.float32)
         for adj_vertex_ind in adjacency_table[vertex_ind]:
             average_vertex += vertices[adj_vertex_ind]
         average_vertex *= 1.0 / len(adjacency_table[vertex_ind])
@@ -35,18 +36,105 @@ def laplacian_smoothing(
     return res
 
 
-def main():
-    vertices = np.array([
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-    ], dtype=np.float32)
-    triangle_indices = [
-        [0, 1, 2]
+def normalize(vector: np.ndarray) -> np.ndarray:
+    length = np.linalg.norm(vector)
+    assert length > 0
+    return vector / length
+
+
+def compute_basis_for_fan(
+        vertices: np.ndarray,
+        normals: np.ndarray,
+        adjacency_table: List[List[int]],
+        vertex_ind: int
+) -> np.ndarray:
+    assert vertices.shape == normals.shape
+    adj_indices = adjacency_table[vertex_ind]
+    assert len(adj_indices) > 0
+    normal = normals[vertex_ind]
+    first_edge = vertices[adj_indices[0]] - vertices[vertex_ind]
+    first_axis = normalize(np.cross(normal, first_edge))
+    second_axis = normalize(np.cross(first_axis, normal))
+    return np.vstack((normal, first_axis, second_axis))
+
+
+def compute_basis_for_mesh(
+        vertices: np.ndarray,
+        normals: np.ndarray,
+        adjacency_table: List[List[int]],
+) -> List[np.ndarray]:
+    assert vertices.shape == normals.shape
+    n_vertices = vertices.shape[0]
+    res = [
+        compute_basis_for_fan(vertices, normals, adjacency_table, vertex_ind)
+        for vertex_ind in range(n_vertices)
     ]
-    adj_table = geom_tools.adjacency_tools.vertices_adjacency_from_polygon_vertex_indices(triangle_indices)
-    res = laplacian_smoothing(vertices, adj_table, 0.5, 10)
-    pass
+    return res
+
+
+def extract_deltas_from_mesh(
+        vertices_to_extract_deltas: np.ndarray,
+        basis_centers: np.ndarray,
+        basis_per_vertex: List[np.ndarray],
+) -> np.ndarray:
+    assumed_shape = vertices_to_extract_deltas.shape
+    n_vertices = assumed_shape[0]
+    assert basis_centers.shape == assumed_shape
+    assert len(basis_per_vertex) == n_vertices
+    res = [
+        np.linalg.inv(basis) @ (vertex - center)
+        for vertex, center, basis in zip(vertices_to_extract_deltas, basis_centers, basis_per_vertex)
+    ]
+    return np.array(res)
+
+
+def apply_deltas(
+        deltas: np.ndarray,
+        basis_centers: np.ndarray,
+        basis_per_vertex: List[np.ndarray],
+) -> np.ndarray:
+    assumed_shape = deltas.shape
+    n_vertices = assumed_shape[0]
+    assert basis_centers.shape == assumed_shape
+    assert len(basis_per_vertex) == n_vertices
+    res = [
+        basis @ delta + center
+        for delta, center, basis in zip(deltas, basis_centers, basis_per_vertex)
+    ]
+    return np.array(res)
+
+
+def main():
+    path_to_src = "/home/daiver/src.obj"
+    path_to_dst = "/home/daiver/dst.obj"
+    path_to_res = "/home/daiver/res.obj"
+
+    smooth_degree = 0.5
+    smooth_iters = 100
+
+    geom_source = geom_tools.load(path_to_src)
+    geom_target = geom_tools.load(path_to_dst)
+    polygon_vertex_indices = geom_source.polygon_vertex_indices
+    triangle_vertex_indices = geom_source.triangle_vertex_indices
+    assert polygon_vertex_indices == geom_target.polygon_vertex_indices
+    adj_table = geom_tools.adjacency_tools.vertices_adjacency_from_polygon_vertex_indices(polygon_vertex_indices)
+    vertices_src = geom_source.vertices
+    vertices_dst = geom_target.vertices
+
+    vertices_src_smooth = laplacian_smoothing(vertices_src, adj_table, smooth_degree, smooth_iters)
+    vertices_dst_smooth = laplacian_smoothing(vertices_dst, adj_table, smooth_degree, smooth_iters)
+    normals_src_smooth = geom_tools.compute_vertices_normals_from_triangles(vertices_src_smooth, triangle_vertex_indices)
+    normals_dst_smooth = geom_tools.compute_vertices_normals_from_triangles(vertices_dst_smooth, triangle_vertex_indices)
+
+    basis_src = compute_basis_for_mesh(vertices_src_smooth, normals_src_smooth, adj_table)
+    basis_dst = compute_basis_for_mesh(vertices_dst_smooth, normals_dst_smooth, adj_table)
+
+    deltas = extract_deltas_from_mesh(vertices_src, vertices_src_smooth, basis_src)
+    res_vertices = apply_deltas(deltas, vertices_dst_smooth, basis_dst)
+    geom_res = deepcopy(geom_source)
+    geom_res.vertices = res_vertices
+    geom_res.normals = geom_tools.compute_vertices_normals_from_triangles(geom_res.vertices, triangle_vertex_indices)
+    geom_tools.save(geom_res, path_to_res)
 
 
 if __name__ == "__main__":
